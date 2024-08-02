2bcfb138e7d2e2b2195f524a497e17c841a85677
Ujjwalsri03 <ujjawalshriwastav9771@gmail.com>
2024-08-02 22:25:08 +0530

    age calculator

--- Age-Calculator-2/script.js
+++ Age-Calculator-2/script.js
@@ -1,37 +1,44 @@
 let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
 
-let years = document.getElementById("years")
-let months = document.getElementById("months")
-let days = document.getElementById("days")
+let years = document.getElementById("years");
+let months = document.getElementById("months");
+let days = document.getElementById("days");
 
 let ageCalculate = document.getElementById("calculate");
 
 ageCalculate.addEventListener("click", () => {
     let birthdate = new Date(userInput.value);
-    if(!isNaN(birthdate)){
+    if (!isNaN(birthdate)) {
         let today = new Date();
 
         let currAge = today.getFullYear() - birthdate.getFullYear();
+        let currMonth = today.getMonth() - birthdate.getMonth();
+        let currDay = today.getDate() - birthdate.getDate();
 
-        let Currmonth = today.getMonth() - birthdate.getMonth() + 1;
-
-        if (Currmonth < 0 || ( Currmonth === 0 && today.getDate() < birthdate.getDate())) {
+        
+        if (currMonth < 0 || (currMonth === 0 && currDay < 0)) {
             currAge--;
         }
 
-        if(Currmonth < 0){
-            Currmonth += 12;
+        if (currMonth < 0) {
+            currMonth += 12;
         }
 
-        let currDay = today.getDate() - birthdate.getDate();
+        const daysInMonth = (year, month) => {
+            return new Date(year, month, 0).getDate();
+        };
 
-
-        // console.log(`currAge ${currAge},Currmonth ${Currmonth},currDay${currDay}`);
+        if (currDay < 0) {
+            currMonth--;
+            if (currMonth < 0) {
+                currMonth += 12;
+            }
+            currDay += daysInMonth(today.getFullYear(), today.getMonth());
+        }
 
         years.innerHTML = currAge;
-        months.innerHTML = Currmonth;
+        months.innerHTML = currMonth;
         days.innerHTML = currDay;
     }
-})
-
+});
